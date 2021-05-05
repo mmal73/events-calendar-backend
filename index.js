2c540cc0296bd6d520d2b8a95e596cef7ef10f45
mmal73 <luisito250899@gmail.com>
2021-05-05 00:25:28 -0500

    feat(express): configs and environment variables

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Project initialized')
+const express = require('express');
+require('dotenv').config();
+
+// Create express server
+const app = express();
+
+app.use( express.static('public') );
+//routes
+app.get('/test', (req, res) => {
+    res.json({
+        status: 'ok'
+    });
+})
+
+// listen to requests
+app.listen( process.env.PORT, () => {
+    console.log(`Example app listening at http://localhost:${process.env.PORT}`)
+});
