95f8efcbf862e5cc7e4601accc4a750e5849d666
mmal73 <luisito250899@gmail.com>
2021-05-05 23:00:48 -0500

    feat(User): register new user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,41 @@
 const express = require('express');
+const bcryptjs = require('bcryptjs');
+const User = require('../models/User');
 
-const register = (req, res = express.response) => {
-    
-    const { name, email, password} = req.body;
+const register = async(req, res = express.response) => {
+    const { email, password} = req.body;
 
-    res.status(201).json({
-        message: 'register',
-        name,
-        email,
-        password,
-    });
+    try {
+        
+        // Check if the user is already registered
+        let user = await User.findOne({ email }); 
+        if( user ){
+            return res.status(400).json({
+                message: 'The user is already registered',
+                status: false,
+            });
+        }
+
+        // Save in db
+        user = new User( req.body );
+        // Hash password
+        user.password = bcryptjs.hashSync(password, 10);
+        await user.save();
+
+        res.status(201).json({
+            message: 'user registered',
+            name: user.name,
+            status: true,
+            _id: user.id,
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            status: false,
+            message: 'An error has ocurred'
+        });
+    }
 
 }
 const login = (req, res = express.response) => {
