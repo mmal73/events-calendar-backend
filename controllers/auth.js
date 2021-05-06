8b50d152a7fb2169d0bcabd4d45e2f337b4ace02
mmal73 <luisito250899@gmail.com>
2021-05-05 23:09:43 -0500

    feat(User): login

--- controllers/auth.js
+++ controllers/auth.js
@@ -23,9 +23,8 @@ const register = async(req, res = express.response) => {
         await user.save();
 
         res.status(201).json({
-            message: 'user registered',
-            name: user.name,
             status: true,
+            name: user.name,
             _id: user.id,
         });
 
@@ -38,15 +37,40 @@ const register = async(req, res = express.response) => {
     }
 
 }
-const login = (req, res = express.response) => {
-
+const login = async(req, res = express.response) => {
     const { email, password} = req.body;
 
-    res.json({
-        message: 'register',
-        email,
-        password,
-    });
+    try {
+        
+        // Check if the user is already registered
+        const user = await User.findOne({ email }); 
+        if( !user ){
+            return res.status(400).json({
+                message: 'The user is not yet registered with this email',
+                status: false,
+            });
+        }
+
+        // Check if the password is correct
+        const correctPassword = bcryptjs.compareSync( password, user.password);
+        if( !correctPassword ){
+            return res.status(400).json({
+                message: 'The password is incorrect',
+                status: false,
+            });
+        }
+        
+        res.json({
+            status: true,
+            name: user.name,
+            _id: user.id,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            status: false,
+        });
+    }
 
 }
 const renewToken = (req, res = express.response) => {
