1c37a2c7c25ba223e1b70e325213df9e686b8b2e
mmal73 <luisito250899@gmail.com>
2021-05-05 22:38:56 -0500

    feat(Model): user

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+module.exports = model('User', userSchema);
